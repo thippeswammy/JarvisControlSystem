f0091f3a31e1f2be86645bf7d408971648cd8d53
Thippeswamy k s <73697198+thippeswammy@users.noreply.github.com>
2024-06-23 20:00:17 +0530

    Delete BardAccess directory

--- BardAccess/B1.py
+++
@@ -1,8 +0,0 @@
-from bardapi import Bard
-
-bard = Bard()
-bard.set_api_key("AIzaSyABmEj2MAVGCx8-6rzSEfmSmUVBM1KtgF0")
-
-message = input("enter your prompt:")
-
-print(bard.get_answer(str(message)))
